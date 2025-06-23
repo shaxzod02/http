841ceedfe2b2eec38ebec81bec8c8b55069f42ae
SHAKHZOD <shoxzodohunjonov@gmail.com>
2025-06-24 07:40:49 +0800

    app.py

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random as rn
 
-N = 500
+N = 2000
 n = 0
 ax = - 2; bx = 2; ay = 0; by = 2
 for i in range(0, N):
@@ -16,7 +16,12 @@ for i in range(0, N):
 
 xx = np.linspace(ax, bx, 100)
 y1 = xx**2; y2 = xx/2 + 1
-plt.plot(xx, y1,linewidth = 3)
-plt.plot(xx, y2)
-
+plt.plot(xx, y1, color = "black", linewidth = 3)
+plt.plot(xx, y2, color = "black", linewidth = 3)
+plt.ylim(ay, by)
 plt.show()
+
+S_toliq = (bx - ax) * (by - ay)
+S_soha = S_toliq * n / N
+
+print("Sohaning yuzi = ", S_soha)
