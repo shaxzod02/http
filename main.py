ad0272af91008ac929f7927cf53e49aa480a7fb4
SHAKHZOD <shoxzodohunjonov@gmail.com>
2025-07-13 10:35:53 +0500

    http

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ STATUS_CODES = {
 }
 
 class Request:
-    def __init__(self, method, path, version, headers = {}):
+    def __init__(self, method, path, version, headers = {}, body = ""):
         self.method = method
         self.path = path
         self.version = version
         self.headers = headers
+        self.body = body
 
     def __str__(self):
         return f"Reuest({self.method} {self.path} HTTP/{self.version})"
@@ -27,10 +28,9 @@ def parse_request_line(req: str) -> Request:
     request_headers_line = parts[0]
     body = parts[1]
 
-    request_line, header_lines = parse_request_line(request_headers_line)
-    method, path, version = parts[0].split(" ")
-    headers = parse_headers(parts[1:])
-    return Request(method, path, version, headers)
+    request_line, headers = parse_request_line(request_headers_line)
+    method, path, version = request_line
+    return Request(method, path, version, headers, body)
 
 
 def parse_request_line(req: str) -> tuple[list, dict]:
@@ -51,6 +51,16 @@ def parse_headers(headers: list[str]) -> dict:
             parsed_headers [key] = value
     return parsed_headers
 
+
+def generate_response(request: Request, body: str) -> str:
+    resp = "HTTP/1.0 200 OK\\r\\n"
+    length = len(body)
+    for key, value in request.headers.items():
+        resp.append(f"{key}: {value}\\r\\n")
+    resp.append(f"Content-Length: {length}\r\n\r\n")
+    resp.append(body)
+    return "".join(resp)
+
         
 def main() -> None:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -64,11 +74,7 @@ def main() -> None:
             while True:
                data = conn.recv(1024)
                request = parse_request_line(data.decode())
-               print(request.method)
-               print(request.version)
-               print(request.path)
-               print(request.headers)
-               print()
+               
                
             #    print("Client:", data)
             #    conn.sendall(f"Echo: {data}".encode())
